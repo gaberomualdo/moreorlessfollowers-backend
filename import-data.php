975e976425ab249dd3059f60438f212fcda1756d
xtrp <36894883+xtrp@users.noreply.github.com>
2021-04-30 20:35:59 -0400

    Remove error catching that turned out to be unfunctional

--- import-data.php
+++ import-data.php
@@ -3,13 +3,8 @@
 include('./auth-token.php');
 
 if(isset($_SERVER['HTTP_X_AUTH_TOKEN']) && $_SERVER['HTTP_X_AUTH_TOKEN'] == $auth_token) {
-  try {
-    $new_accounts = json_decode(file_get_contents('php://input'), true);
-    file_put_contents('accounts.json', json_encode($new_accounts));
-  } catch(Exception $e) {
-    http_response_code(500);
-    echo json_encode([ "message" => "A server error occurred." ]);
-  }
+  $new_accounts = json_decode(file_get_contents('php://input'), true);
+  file_put_contents('accounts.json', json_encode($new_accounts));
 } else {
   http_response_code(401);
   echo json_encode([ "message" => "Missing or bad authorization token." ]);
